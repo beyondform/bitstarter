1802ec85f4d3da87d440427d3600e26da3f0848c
Ubuntu <ubuntu@ip-10-40-141-177.ec2.internal>
2013-07-11 04:11:19 +0000

    and another try

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('./bitstarter/index.html', 'utf8');
+
 app.get('/', function(request, response) {
 
-var content = fs.readFileSync('./bitstarter/index.html', 'utf8');
 
   response.send( content ); 
 });
